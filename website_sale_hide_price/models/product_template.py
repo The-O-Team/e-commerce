d3e2aac650607471d16ed9f4769447bb0f8d0370
david <david.vidal@tecnativa.com>
2022-05-13 13:27:25 +0200

    [IMP] website_sale_hide_price: hide price at product level
    No we can hide the price only for the products we want to.
    
    TT36129

--- website_sale_hide_price/models/product_template.py
+++ website_sale_hide_price/models/product_template.py
@@ -19,6 +19,7 @@ class ProductTemplate(models.Model):
         combination=False,
         product_id=False,
         add_qty=1,
+        pricelist=False,
         parent_combination=False,
         only_template=False,
     ):
@@ -26,6 +27,7 @@ class ProductTemplate(models.Model):
             combination=combination,
             product_id=product_id,
             add_qty=add_qty,
+            pricelist=pricelist,
             parent_combination=parent_combination,
             only_template=only_template,
         )
@@ -36,22 +38,3 @@ class ProductTemplate(models.Model):
             }
         )
         return combination_info
-
-    def _search_render_results(self, fetch_fields, mapping, icon, limit):
-        """Hide price on the search bar results"""
-        results_data = super()._search_render_results(
-            fetch_fields, mapping, icon, limit
-        )
-        website_show_price = (
-            self.env["website"].get_current_website().website_show_price
-        )
-        for product, data in zip(self, results_data, strict=True):
-            if product.website_hide_price or not website_show_price:
-                data.update(
-                    {
-                        "price": "<span>%s</span>"
-                        % (product.website_hide_price_message or ""),
-                        "list_price": "",
-                    }
-                )
-        return results_data
--- website_sale_hide_price/models/res_config_settings.py
+++ website_sale_hide_price/models/res_config_settings.py
@@ -6,12 +6,6 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    website_hide_price = fields.Boolean(
-        string="Hide prices on website",
-        related="website_id.website_hide_price",
-        readonly=False,
-    )
     website_hide_price_default_message = fields.Char(
-        related="website_id.website_hide_price_default_message",
-        readonly=False,
+        related="website_id.website_hide_price_default_message", readonly=False,
     )
